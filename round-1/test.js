6dc810fb4baf1f44c65d91ca5959f811218af164
Alexandr Plesovskih <hidrogen99@ya.ru>
2013-11-12 02:14:42 +0600

    Оно шевелится

--- round-1/test.js
+++ round-1/test.js
@@ -1,5 +1,10 @@
 test( "Test game of life", function() {
+    // Создаем экземпляр игры
     var g = new GameOfLife();
+    // Пытаемся зародить жизнь
     g.setCell(0, 0);
-    ok(g.getCell === true, "Клетка живет!" );
+    // Узнаем как у нас клеточка поживает
+    var status = g.getCell(0, 0);
+    // Проверяем полученный статус
+    ok(status === true, "Оно шевелится!");
 });
