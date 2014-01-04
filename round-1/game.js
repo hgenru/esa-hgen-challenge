1ae025dc9d9c188126faba1dbace2971e857f166
korewaESA <korewaesa@mail.ru>
2014-01-05 02:44:09 +0400

    Нунаконецто1

---
+++ round-1/game.js
@@ -0,0 +1,22 @@
+function GameOfLife()
+{
+	var Cells = [];
+	var this.M = 20;
+	var this.N = 20;
+	for (var i = 0; i < m; i++)
+		{
+			mas[i] = [];
+			for (var j = 0; j < n; j++)
+				{
+					mas[i][j] = false;
+				}
+		}
+	function setCell(x,y)
+	{
+		this.Cells[x][y] = true;
+	}
+	function getCell(x,y)
+	{
+		if this.Cells[x][y] === true then return true else return false;
+	}
+}
