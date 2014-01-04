726ddb0aeb4ce3c829abc9aace2aea74a273e05f
Alexander Plesovskikh <me@hgen.ru>
2014-01-05 05:22:24 +0600

    Будем проверять живучесть

--- round-1/test.js
+++ round-1/test.js
@@ -1,10 +1,24 @@
-test("Test game of life", function() {
+module( "Test Game Of Life", {
+  setup: function() {
     // Создаем экземпляр игры
-    var g = new GameOfLife();
+    this.game = new GameOfLife();
+  }
+});
+test("test set and get", function() {
     // Пытаемся зародить жизнь
-    g.setCell(0, 0);
+    this.game.setCell(0, 0);
     // Узнаем как у нас клеточка поживает
-    var status = g.getCell(0, 0);
+    var status = this.game.getCell(0, 0);
     // Проверяем полученный статус
     ok(status === true, "Оно шевелится!");
+});
+test("test set and get", function() {
+    // Пытаемся зародить жизнь
+    this.game.setCell(0, 0);
+    // Переходим на следующий шаг
+    this.game.nextStep();
+    // Узнаем как у нас клеточка поживает
+    var status = this.game.getCell(0, 0);
+    // Проверяем полученный статус
+    ok(status === false, "Оно шевелилось, но умерло :с");
 });
