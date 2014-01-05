63643130a32da813f6400d4f4001f3b1c4528dff
korewaESA <korewaesa@mail.ru>
2014-01-05 07:40:39 +0400

    13
    Смотри это работает, но у тебя тест не правильный
    Там вместо
    (this.game.getCell(1, 0) &&
    this.game.getCell(0, 0) &&
    this.game.getCell(-1, 0))
    Должно быть
    (this.game.getCell(0, 1) &&
    this.game.getCell(0, 0) &&
    this.game.getCell(0, -1))

--- round-1/game.js
+++ round-1/game.js
@@ -1,8 +1,9 @@
 function GameOfLife()
 {
 	this.Cells = [];
-	M = 4;
-	N = 4;
+	this.tCells = [];
+	M = 6;
+	N = 6;
 	for (var i = 0; i < M; i++)
 		{
 			this.Cells[i] = [];
@@ -11,53 +12,65 @@ function GameOfLife()
 					this.Cells[i][j] = false;
 				}
 		}
-
-	this.CheckCell = function(x,y)
-	{
-		function thorX(x)
+	function thorX(x)
 		{
 			if (x<0) {return(x+M)}
 			if (x>=M) {return(x-M)}
 			return x
 		}
-		function thorY(y)
+	function thorY(y)
 		{
 			if (y<0) {return y+N}
 			if (y>=N) {return y-N}
 			return y
 		}
-
+	this.CheckCell = function(x,y)
+	{
 		var t=0;
 		for (var i = x-1; i <= x+1; i++)
 		{
 			for (var j = y-1; j <= y+1; j++)
 				{
-					//console.log(x,y,i,j,thorX(i),thorY(j),t);
-					if (this.Cells[thorX(i)][thorY(j)] === true) {t++}
+					//console.log(x,y,i,j,t,this.tCells[thorX(i)][thorY(j)]);
+					if (this.tCells[thorX(i)][thorY(j)] === true) {t++}
 				}
 		}
-		if (this.Cells[x][y] === true) {t--}
-		//console.log(x,y,t);
-		if (t === 3) {this.Cells[thorX(x)][thorY(y)]=true} else if (t !== 2) {this.Cells[thorX(x)][thorY(y)]=false}
+		if (this.tCells[x][y] === true) {t--}
+		//console.log(x,y,thorX(x),thorY(y),t);
+		if (t === 3) {this.setCell(x,y)} else if (t !== 2) {this.dellCell(x,y)}
 	}
-
+    this.dellCell = function (x,y)
+	{
+		this.Cells[thorX(x)][thorY(y)] = false;
+	}
 	this.setCell = function (x,y)
 	{
-		this.Cells[x][y] = true;
+		this.Cells[thorX(x)][thorY(y)] = true;
 	}
 	this.getCell = function (x,y)
 	{
-		if (this.Cells[x][y] === true) {return true} else {return false};
+		if (this.Cells[thorX(x)][thorY(y)] === true) {return true} else {return false};
 	}
 	this.nextStep = function ()
 	{
+		//console.log("!");
 		for (var i = 0; i < M; i++)
 		{
+			this.tCells[i] = [];
+			for (var j = 0; j < N; j++)
+				{
+					this.tCells[i][j] = this.Cells[i][j];
+				}
+		}
+
+		for (var i = 0; i < M; i++)
+		{
+			//console.log("Do",this.tCells[i]);
 			for (var j = 0; j < N; j++)
 				{
 					this.CheckCell(i,j);
 				}
-			//console.log(this.Cells[i]);
+			//console.log("Po",this.Cells[i]);
 		}
 	}
 }
