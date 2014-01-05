41f58797fd032680c78ae307dac977e848d302c3
Alexander Plesovskikh <me@hgen.ru>
2014-01-05 07:32:21 +0600

    Квадрат непоколебим!

--- round-1/test.js
+++ round-1/test.js
@@ -1,8 +1,8 @@
 module( "Test Game Of Life", {
-  setup: function() {
-    // Создаем экземпляр игры
-    this.game = new GameOfLife();
-  }
+    setup: function() {
+        // Создаем экземпляр игры
+        this.game = new GameOfLife();
+    }
 });
 test("test set and get", function() {
     // Пытаемся зародить жизнь
@@ -21,4 +21,23 @@ test("test next step", function() {
     var status = this.game.getCell(0, 0);
     // Проверяем полученный статус
     ok(status === false, "Оно шевелилось, но умерло :с");
+});
+test("test square", function() {
+    // Пытаемся зародить жизнь
+    this.game.setCell(0, 0);
+    this.game.setCell(0, 1);
+    this.game.setCell(1, 0);
+    this.game.setCell(1, 1);
+    // Переходим на следующий шаг
+    this.game.nextStep();
+    // Переходим еще на шаг для надежности
+    this.game.nextStep();
+    // Узнаем как поживает наш квадрат
+    ok(
+        (this.game.getCell(0, 0) &&
+         this.game.getCell(0, 1) &&
+         this.game.getCell(1, 0) &&
+         this.game.getCell(1, 1)),
+        "Оно жило и будет жить!"
+    );
 });
