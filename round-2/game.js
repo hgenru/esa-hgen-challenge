82874e257dec1ecb45629f03c0ed05304556f088
korewaESA <korewaesa@mail.ru>
2014-01-07 00:17:33 +0400

    r2s5

--- round-2/game.js
+++ round-2/game.js
@@ -26,6 +26,7 @@ function GameOfLife()
 	this.isAlive = function(x,y)
 	{
 		var t=0;
+		var v=false;
 		for (var i=0; i<this.p; i++)
 		{
 			if ((this.Cells[i][0]>x-2)&&(this.Cells[i][0]<x+2)&&
@@ -33,22 +34,32 @@ function GameOfLife()
 				{
 					t++;
 				}
-		}
-		if ((t=2) || (t=3)) {return true} else {return false}; 
+			if ((this.Cells[i][0]===x)&&(this.Cells[i][1]===y)) 
+				{
+					v=true;
+					t--;
+				}
+		};
+		//console.log(t,v);
+		if (((t===2)&&(v===true))||(t===3)){return true}else{return false}; 
 	}
 
 	this.nextStep = function()
 	{
 		var nsCells = [];
 		var nsp=0;
-		for (var i=0; i<this.p; i++)
+		for (var k=0; k<this.p; k++)
 		{
-
-			if (this.isAlive(this.Cells[i][0],this.Cells[i][1]))
-			{
-				nsCells[nsp] = this.Cells[i];
-				nsp++;
-			}
+			for (var i=this.Cells[k][0]-1; i<this.Cells[k][0]+2; i++)
+				for (var j=this.Cells[k][1]-1; j<this.Cells[k][1]+2; j++)
+				{
+					if (this.isAlive(i,j))
+					{
+						//console.log("nsp=",nsp,this.isAlive(i,j));
+						nsCells[nsp] = [i,j];
+						nsp++;
+					}
+				}
 		}		
 		this.Cells.length = 0;
 		this.p=nsp;
