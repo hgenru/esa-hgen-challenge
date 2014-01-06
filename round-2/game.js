a6062d9a84b561b82df87a52949e74c287b05558
korewaESA <korewaesa@mail.ru>
2014-01-06 22:37:11 +0400

    r2s2

--- round-2/game.js
+++ round-2/game.js
@@ -2,27 +2,29 @@ function GameOfLife()
 {
 	this.Cells = [];
 	this.p = 0;
+
 	this.setCell = function(x,y)
 	{
-		this.Cells[p].x = x;
-		this.Cells[p].y = y;
-		this.p++;
+		this.Cells[this.p] = [x,y];
+		this.p = this.p + 1;
 	}
 
 	this.getCell = function(x,y)
 	{
+		
 		for (var i=0; i<this.p; i++)
 		{
-			if ((this.Cells[i].x===x)&&(this.Cells[i].y===y)) 
+			if ((this.Cells[i][0]===x)&&(this.Cells[i][1]===y)) 
 				{
 					return true;
-				}
-		}	
+				};
+		};	
 		return false;
 	}
 
 	this.isAlive = function(x,y)
 	{
+		/*
 		var t=0;
 		for (var i=0; i<this.p; i++)
 		{
@@ -32,6 +34,6 @@ function GameOfLife()
 					t++;
 				}
 		}
-		if ((t=2) || (t=3)) {return true} else {return false};
+		if ((t=2) || (t=3)) {return true} else {return false}; */
 	}
 }
